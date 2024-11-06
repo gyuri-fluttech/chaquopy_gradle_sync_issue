498dce11a87f7f35898a3d06524684804aefe4d7
Gyuri Majercsik <gyuri@fluttech.com>
2024-11-06 10:38:26 +0200

    Android project with small reference to Flutter library.

---
+++ app/src/main/java/com/fluttech/chaquopy_gradle_sync_issue/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.fluttech.chaquopy_gradle_sync_issue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fluttech.chaquopy_gradle_sync_issue.ui.theme.Chaquopy_gradle_sync_issueTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Chaquopy_gradle_sync_issueTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Chaquopy_gradle_sync_issueTheme {
+        Greeting("Android")
+    }
+}
